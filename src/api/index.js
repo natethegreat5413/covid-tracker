4a5a0598214da14fd675656c595b5e11bedae8ec
Nathan Cowley <nathancowley12@gmail.com>
2020-12-23 17:31:41 -0700

    finished with the cards component

---
+++ src/api/index.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const url = 'https://covid19.mathdro.id/api';
+
+export const fetchData = async () => {
+	try {
+		const {
+			data: { confirmed, recovered, deaths, lastUpdate }
+		} = await axios.get(url);
+
+		return { confirmed, recovered, deaths, lastUpdate };
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+//// WITHOUT DESTRUCTURING ////
+// import axios from 'axios';
+
+// const url = 'https://covid19.mathdro.id/api';
+
+// export const fetchData = async () => {
+// 	try {
+// 		const { data } = await axios.get(url);
+
+// 		const modifiedObject = {
+// 			confirmed: data.confirmed,
+// 			recovered: data.recovered,
+// 			deaths: data.deaths,
+// 			lastUpdate: data.lastUpdate
+// 		};
+// 		return modifiedObject;
+// 	} catch (error) {
+// 		console.log(error);
+// 	}
+// };
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+// This makes it so you don't have to make a separate import for each individual component
+export { default as Cards } from './Cards/Cards';
+export { default as Chart } from './Chart/Chart';
+export { default as CountryPicker } from './CountryPicker/CountryPicker';
